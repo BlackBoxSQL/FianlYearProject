7dbfa802d536ab04c57deb948b02ad3e352b57b2
BlackBoxSQL <pythonboy007@gmail.com>
2019-01-25 00:46:05 +0600

    Guardian & Tutor profile (workingv2)

--- hiretutor/hireapp/models.py
+++ hiretutor/hireapp/models.py
@@ -16,7 +16,7 @@ class TutorProfiles(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     Your_Full_Name = models.CharField(max_length=50)
     Fathers_Name = models.CharField("Father's Name", max_length=50)
-    Mothers_Name = models.CharField("Mother's Name",max_length=50)
+    Mothers_Name = models.CharField("Mother's Name", max_length=50)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     age = models.CharField(max_length=3)
     Date_of_Birth = models.DateField()
@@ -25,7 +25,7 @@ class TutorProfiles(models.Model):
     nid_No = models.CharField(max_length=18)
     tution_you_give_to_class = models.CharField(max_length=2)
     subjects_you_teach = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField("Contact Number", max_length=14)
     need_tution_from = models.DateField()
     fees_per_subject = models.CharField(max_length=5)
     week = models.CharField("How many days you are available?", max_length=1)
@@ -35,19 +35,24 @@ class TutorProfiles(models.Model):
     def __str__(self):
         return self.Your_Full_Name
 
-    def get_queryset(self):
-        return TutorProfiles.objects.all()
-
 
 class GuardianProfiles(models.Model):
+    GENDER_CHOICES = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    name = models.CharField(max_length=200)
-    age = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='', blank=True, null=True)
-    g_or_t = models.CharField(max_length=20)
-
+    Guardians_name = models.CharField("Guardian's Name", max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    nid_No = models.CharField(max_length=18)
+    occupation = models.CharField(max_length=20)
+    students_name = models.CharField("Student's Name", max_length=50)
+    s_class = models.CharField("Student's Class", max_length=2)
+    school = models.CharField("School/College", max_length=60)
+    subject = models.CharField(max_length=100)
+    payment = models.CharField(max_length=5)
+    tution_hour = models.CharField("Tution hours", max_length=2)
+    guardian_contact = models.CharField("Guardian's Contact Number", max_length=14)
+    need_tutor_from = models.DateField()
     def __str__(self):
-        return self.name
-
-    def get_queryset(self):
-        return GuardianProfiles.objects.all()
+        return self.Full_name
