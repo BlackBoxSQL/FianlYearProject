aeb5b4d4d26492080329ca5bd079f044b069d2c4
BlackBoxSQL <pythonboy007@gmail.com>
2019-01-26 23:42:08 +0600

    Guardian & Tutor profile (workingv2)

--- hiretutor/hireapp/models.py
+++ hiretutor/hireapp/models.py
@@ -10,12 +10,12 @@ class User(AbstractUser):
 
 class TutorProfiles(models.Model):
     GENDER_CHOICES = (
-        ('m', 'Male'),
-        ('f', 'Female'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     Your_Full_Name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
     age = models.CharField(max_length=3)
     Date_of_Birth = models.DateField()
     image = models.ImageField(upload_to='', blank=True, null=True)
@@ -27,7 +27,7 @@ class TutorProfiles(models.Model):
     need_tution_from = models.DateField()
     fees_per_subject = models.CharField(max_length=5)
     week = models.CharField("How many days you are available?", max_length=1)
-    tution_hour = models.CharField(max_length=2)
+    tution_hour = models.CharField(max_length=3)
     description = models.TextField(max_length=250)
 
     def __str__(self):
@@ -36,12 +36,12 @@ class TutorProfiles(models.Model):
 
 class GuardianProfiles(models.Model):
     GENDER_CHOICES = (
-        ('m', 'Male'),
-        ('f', 'Female'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     Guardians_name = models.CharField("Guardian's Name", max_length=50)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
     nid_No = models.CharField(max_length=18)
     occupation = models.CharField(max_length=20)
     students_name = models.CharField("Student's Name", max_length=50)
@@ -49,7 +49,7 @@ class GuardianProfiles(models.Model):
     school = models.CharField("School/College", max_length=60)
     subject = models.CharField(max_length=100)
     payment = models.CharField(max_length=5)
-    tution_hour = models.CharField("Tution hours", max_length=2)
+    tution_hour = models.CharField("Tution hours", max_length=3)
     guardian_contact = models.CharField("Guardian's Contact Number", max_length=14)
     need_tutor_from = models.DateField()
     description = models.TextField(max_length=250)
