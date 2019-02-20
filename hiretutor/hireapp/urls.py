4167e684bf8088a692e8cc7798b3dce611d9129a
BlackBoxSQL <naziburrahmanrockycop@gmail.com>
2019-02-20 16:14:02 +0600

    filter try1

--- hiretutor/hireapp/urls.py
+++ hiretutor/hireapp/urls.py
@@ -1,3 +1,14 @@
+"""hiretutor URL Configuration
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.urls import include, path
 
 from .views import hireapp, tutor, guardian
@@ -10,6 +21,8 @@ urlpatterns = [
                                         name='guardian_homepage'),
                                    path('guardianprofile/', guardian.GuardianProfile.as_view(),
                                         name='guardian_profile'),
+                                   path('guardiansearch/', guardian.search_tutor,
+                                        name='searchtutor'),
                                ], 'hireapp'), namespace='guardian')),
     path('tutor/', include(([
                                 path('tutorhomepage/', tutor.TutorHomepage.as_view(),
